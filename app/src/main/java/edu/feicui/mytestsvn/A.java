ad394085c6d0954a15e0ee6c6a2d7660a06f60fc
chentong <chentong217@163.com>
2016-10-19 14:02:28 +0800

    Initial commit

---
+++ app/src/main/java/edu/feicui/mytestsvn/A.java
@@ -0,0 +1,9 @@
+package edu.feicui.mytestsvn;
+
+/**
+ * Created by Administrator on 2016/10/18.
+ *https://huangyi/svn/TestCode/
+ *
+ */
+public class A {
+}
---
+++ app/src/main/java/edu/feicui/mytestsvn/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.feicui.mytestsvn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
